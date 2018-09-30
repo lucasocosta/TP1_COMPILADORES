7c35e8fdf66eec065a8a29a9fa981613035fa743
Escarassatti <38073312+Thaycondrius@users.noreply.github.com>
2018-09-30 01:51:19 -0300

    Chamadas Sintatico
    Chamadas feitas

--- src/analisadorSintatico/AnalisadorSintatico.java
+++ src/analisadorSintatico/AnalisadorSintatico.java
@@ -17,4 +17,105 @@ public class AnalisadorSintatico {
     {
         
     }
+    
+    public void Programa(List<Token> tokens)
+    {
+        
+    }
+    public void Decl_Comando (List<Token> tokens)
+    {
+        
+    }
+    public void Declaracao (List<Token> tokens)
+    {
+        
+    }
+    public void Decl2 (List<Token> tokens)
+    {
+        
+    }
+    public void Tipo (List<Token> tokens)
+    {
+        
+    }
+    public void Comando (List<Token> tokens)
+    {
+        
+    }
+    public void Bloco (List<Token> tokens)
+    {
+        
+    }
+    public void Atribuicao (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoSe (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoSenao (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoEnquanto (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoRead (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoPrint (List<Token> tokens)
+    {
+        
+    }
+    public void ComandoFor (List<Token> tokens)
+    {
+        
+    }
+    public void AtribuicaoFor (List<Token> tokens)
+    {
+        
+    }
+    public void Expressao (List<Token> tokens)
+    {
+        
+    }
+    public void RelacaoOpc (List<Token> tokens)
+    {
+        
+    }
+    public void OpRel (List<Token> tokens)
+    {
+        
+    }
+    public void Adicao (List<Token> tokens)
+    {
+        
+    }
+    public void AdicaoOpc (List<Token> tokens)
+    {
+        
+    }
+    public void OpAdicao (List<Token> tokens)
+    {
+        
+    }
+    public void Termo (List<Token> tokens)
+    {
+        
+    }
+    public void TermoOpc (List<Token> tokens)
+    {
+        
+    }
+    public void OpMult (List<Token> tokens)
+    {
+        
+    }
+    public void Fator (List<Token> tokens)
+    {
+        
+    }
 }
