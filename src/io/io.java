87a1ea7a58198a47a3c0535a01ad906ef47a7969
lucasocosta <lucasocosta@hotmail.com>
2018-10-14 19:52:51 -0300

    tentando salvar

--- src/io/io.java
+++ src/io/io.java
@@ -5,10 +5,14 @@
  */
 package io;
 
+import analisadorSintatico.TabelaSimbolos;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
@@ -18,8 +22,14 @@ public class io {
     private String buffer;
     private String arquivo;
     private FileReader fr;
+    private FileWriter fw;
     private BufferedReader bfr;
+    private BufferedWriter bfw;
     
+    public io()
+    {
+        
+    }
     
     public io(String arquivo) throws IOException
     {
@@ -27,6 +37,20 @@ public class io {
         this.buffer="";
         fr=new FileReader(arquivo);
         bfr = new BufferedReader(fr);
+    }
+    
+    public void tabSimbolos(String arquivo, TabelaSimbolos tabSimbolos) throws IOException
+    {
+        arquivo=this.arquivo;
+        fw = new FileWriter(arquivo);
+        PrintWriter pw = new PrintWriter(fw);
+        pw.print("\nTabela de Simbolos\nLex\tTipo\tValor\tLinha\n");
+        for(int i=0;i<tabSimbolos.tamanho();i++)
+        {
+            pw.print(tabSimbolos.get(i).getLexema()+"\t"+tabSimbolos.get(i).getTipo()+"\t"+
+                    tabSimbolos.get(i).getValor()+"\t"+tabSimbolos.get(i).getLinha()+"\n");
+        }
+        fw.close();
     }
     
     public void fechar_arquivo() throws IOException
