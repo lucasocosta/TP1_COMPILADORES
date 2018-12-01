762c7ab4b1aad79817f7a7919673751f66698d75
lucasocosta <lucasocosta@hotmail.com>
2018-12-01 11:35:35 -0200

    declaração dos tipos nós AST

---
+++ src/ast/ArithOp.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+/**
+ *
+ * @author lucas
+ */
+public class ArithOp extends No{
+    
+    public ArithOp(String nome) {
+        super(nome);
+        System.out.print("Criando no do tipo ArithOp");
+    }
+    
+}
+
---
+++ src/ast/LogicalOp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ast;
+
+/**
+ *
+ * @author lucas
+ */
+public class LogicalOp extends No{
+    
+    public LogicalOp(String nome) {
+        super(nome);
+        System.out.print("Criando no do tipo LogicalOp");
+    }
+    
+}
